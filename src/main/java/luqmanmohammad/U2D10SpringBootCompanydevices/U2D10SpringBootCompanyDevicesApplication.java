c855dc16e1fc9a0d0f3fcc7bda8972fa7c583cda
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-06-09 09:42:32 +0100

    Initial commit

---
+++ src/main/java/luqmanmohammad/U2D10SpringBootCompanydevices/U2D10SpringBootCompanyDevicesApplication.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.U2D10SpringBootCompanydevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2D10SpringBootCompanyDevicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2D10SpringBootCompanyDevicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luqmanmohammad/U2D10SpringBootCompanydevices/U2D10SpringBootCompanyDevicesApplicationTests.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.U2D10SpringBootCompanydevices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2D10SpringBootCompanyDevicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
